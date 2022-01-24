cfecdf338bb992dc4984a9e85b607d6d15518f61
GitHubSci <Sci_HomeOffice@t-online.de>
2022-01-24 14:48:16 +0100

    sechster commit

--- Hello_World_1.c
+++ Hello_World_1.c
@@ -5,7 +5,7 @@ int main(void)
 	printf(" Kommt der jetzt immer noch durch oder nicht ? \n");
 	printf(" klappt der Rückweg ? \n");
 	printf(" Ja - hat geklappt !! \n");
-	printf(" ... Super ... !! \n");	
+	printf(" ... Super und gut... !! \n");	
 return 0;
 }
  
